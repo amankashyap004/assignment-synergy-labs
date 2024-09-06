50311b8dabc0db8f6d862b604ca2c3ee4da63b94
amankashyap004 <amankashyap004@gmail.com>
2024-09-06 14:02:13 +0530

    install and setup: dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import UserDetails from "./pages/UserDetails";
 
 function App() {
-  
-
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/user",
+      element: <UserDetails />,
+    },
+  ]);
   return (
-    <main>
-    <p>Assignment Synergy Labs</p>
-   </main>
-  )
+    <main className="font-poppins">
+      <Header />
+      <RouterProvider router={router} />
+      <Footer />
+    </main>
+  );
 }
 
-export default App
+export default App;
