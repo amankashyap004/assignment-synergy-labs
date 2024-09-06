3249442f1b5283b9543e41d01450f863847ef304
amankashyap004 <amankashyap004@gmail.com>
2024-09-06 16:16:59 +0530

    add: header and footer components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer: React.FC = () => {
   return (
-    <div>Footer</div>
+   <footer className="container px-4 lg:px-10 py-4 lg:py-6">
+   <div className='flex justify-center items-center'>
+   <p className='text-sm lg:text-base opacity-60 font-semibold'>&copy; {new Date().getFullYear()}  | All rights reserved.</p>
+   </div>
+   </footer>
   )
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Header: React.FC = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className=" py-4 lg:py-6 shadow-lg w-full fixed top-0 z-50 bg-white">
+      <nav className="container px-4 lg:px-10 flex justify-between items-center">
+        <a
+          href={"/"}
+          className="font-semibold text-lg lg:text-2xl font-plusJakartaSans"
+        >
+          TODO
+        </a>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
